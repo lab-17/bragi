5b1522c318ef447faf8758633d685059c6b4be9f
Vinícius Carvalho <18464132+Oak-V@users.noreply.github.com>
2020-09-08 20:36:13 -0300

    Devtools (#30)
    * build(eslint): update ecmascript version
    
    * build(test): add coverage
    
    * ci(actions): add on push verify
    
    * ci(actions): add pull request verify
    
    * ci(actions): update docs workflow
    
    * ci(actions): add codeclimate coverage workflow
    
    * build(git): fix gitignore
    
    * build(deps): update dependencies and modify scripts
    
    * build(vscode): hidden config files and ignore generated files in search
    
    * test(jest): update jest config
    
    Co-authored-by: Vinícius Carvalho <vinicius@cisi.coppe.ufrj.br>
    Co-authored-by: Vinícius Carvalho <vinicius@criptext.com>

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
     preset: 'ts-jest',
-    clearMocks: true,
-    coverageDirectory: 'coverage',
     coverageProvider: 'v8',
+    coverageReporters: ['lcov', 'text-summary'],
+    collectCoverageFrom: ['src/**/*.ts'],
+    coveragePathIgnorePatterns: [`\\/(types|index)\\.ts$`],
+    watchPathIgnorePatterns: ['.*\\.(js|md|yml|json|lock)$', '^\\..*', '^(audio|coverage|lib)\\/*'],
+    // coverageThreshold: {
+    //     global: {
+    //         branches: 80,
+    //         functions: 80,
+    //         lines: 80,
+    //         statements: -10,
+    //     },
+    // },
 }
