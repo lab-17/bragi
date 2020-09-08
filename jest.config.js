ace6ddaac5094bdcfcc10e58ac5844eca6a4d34b
Vinícius Carvalho <vinicius@criptext.com>
2020-09-08 20:31:15 -0300

    test(jest): update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
     preset: 'ts-jest',
-    coverageDirectory: 'coverage',
     coverageProvider: 'v8',
-    coverageReporters: ['lcov', 'text'],
+    coverageReporters: ['lcov', 'text-summary'],
     collectCoverageFrom: ['src/**/*.ts'],
-    coveragePathIgnorePatterns: [`\\/types\\.ts$`, `\\/index\\.ts$`],
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: -10,
-        },
-    },
+    coveragePathIgnorePatterns: [`\\/(types|index)\\.ts$`],
+    watchPathIgnorePatterns: ['.*\\.(js|md|yml|json|lock)$', '^\\..*', '^(audio|coverage|lib)\\/*'],
+    // coverageThreshold: {
+    //     global: {
+    //         branches: 80,
+    //         functions: 80,
+    //         lines: 80,
+    //         statements: -10,
+    //     },
+    // },
 }
