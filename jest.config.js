13196c302b3b42fca67fddad17c8ae9dfcd14cf9
Vinícius Carvalho <vinicius@cisi.coppe.ufrj.br>
2020-09-08 16:03:30 -0300

    build(test): add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
     preset: 'ts-jest',
-    clearMocks: true,
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
+    coverageReporters: ['lcov', 'text'],
+    collectCoverageFrom: ['src/**/*.ts'],
+    coveragePathIgnorePatterns: [`\\/types\\.ts$`, `\\/index\\.ts$`],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: -10,
+        },
+    },
 }
