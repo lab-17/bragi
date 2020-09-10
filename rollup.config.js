ce98aba838d4ac0883412275752b26d232f69ef1
Vinícius Carvalho <vinicius@criptext.com>
2020-09-10 00:47:45 -0300

    chore(rollup): rollup configuration refactored
    refactoring rollup configuration file to reduce code repetition
    
    fix #28 #29

--- rollup.config.js
+++ rollup.config.js
@@ -29,154 +29,95 @@ const legacySupport = `cover 99.5% and ${banedSupport}`
 const modernSupport = `${legacySupport} and supports es6-module`
 const nextSupport = `${modernSupport} and >=0.2% and last 3 major versions and last 5 versions`
 
-export default [
-    {
-        input,
-        plugins: [typescript(), strip()],
-        output: [
-            {
-                ...nodeConfig,
-                format: 'esm',
-                dir: `${outputDir}/esm`,
-                plugins: [
-                    babel({
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: {
-                                        browsers: nextSupport,
-                                        node: true,
-                                    },
-                                },
-                            ],
-                        ],
-                    }),
+const nodeLegacySupport = 'maintained node versions'
+
+const node = {
+    input,
+    plugins: [typescript(), strip()],
+    output: [
+        { format: 'esm', browsers: nextSupport, node: true },
+        { format: 'cjs', exports: 'named' },
+    ].map(createNodeOutputConfig),
+}
+
+const typeDefinitions = {
+    input,
+    plugins: [dts()],
+    output: [
+        {
+            ...nodeConfig,
+            sourcemap: false,
+            format: 'esm',
+            dir: `${outputDir}/types`,
+            plugins: [],
+        },
+    ],
+}
+
+const browsers = {
+    input,
+    plugins: [typescript(), strip(), terser()],
+    output: [
+        { format: 'esm', modules: 'auto', browsers: modernSupport },
+        { name: filename, format: 'umd' },
+        { format: 'system', modules: 'systemjs' },
+        { format: 'amd' },
+    ].map(createBrowserOutputConfig),
+}
+
+console.log(JSON.stringify([node, typeDefinitions, browsers], null, 2))
+
+export default [node, typeDefinitions, browsers]
+
+function createNodeOutputConfig({ format, browsers, node, exports }) {
+    const targets = {
+        browsers: browsers ?? nodeLegacySupport,
+    }
+
+    if (node) targets.node = node
+
+    return {
+        ...nodeConfig,
+        format,
+        dir: `${outputDir}/${format}`,
+        exports,
+        plugins: [
+            babel({
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            targets,
+                        },
+                    ],
                 ],
-            },
-            {
-                ...nodeConfig,
-                format: 'cjs',
-                exports: 'named',
-                dir: `${outputDir}/cjs`,
-                plugins: [
-                    babel({
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: { browsers: 'maintained node versions' },
-                                },
-                            ],
-                        ],
-                    }),
-                ],
-            },
+            }),
         ],
-    },
-    {
-        input,
-        plugins: [dts()],
-        output: [
-            {
-                preserveModules: true,
-                preferConst: true,
-                format: 'esm',
-                dir: `${outputDir}/types`,
-                plugins: [],
-            },
+    }
+}
+
+function createBrowserOutputConfig({ name, format, modules, browsers, exports }) {
+    const babelPlugin = babel({
+        allowAllFormats: true,
+        presets: [
+            [
+                '@babel/preset-env',
+                {
+                    modules: modules ?? format,
+                    targets: {
+                        browsers: browsers ?? legacySupport,
+                    },
+                },
+            ],
         ],
-    },
-    {
-        input,
-        plugins: [typescript(), strip(), terser()],
-        output: [
-            {
-                ...browserConfig,
-                format: 'esm',
-                file: `${outputDir}/bundle/esm/${filename}.js`,
-                plugins: [
-                    babel({
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: {
-                                        browsers: modernSupport,
-                                    },
-                                },
-                            ],
-                        ],
-                    }),
-                ],
-            },
-            {
-                ...browserConfig,
-                name: filename,
-                format: 'umd',
-                exports: 'named',
-                file: `${outputDir}/bundle/umd/${filename}.js`,
-                plugins: [
-                    babel({
-                        allowAllFormats: true,
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    modules: 'umd',
-                                    targets: {
-                                        browsers: legacySupport,
-                                    },
-                                },
-                            ],
-                        ],
-                    }),
-                ],
-            },
-            {
-                ...browserConfig,
-                format: 'system',
-                exports: 'named',
-                file: `${outputDir}/bundle/system/${filename}.js`,
-                plugins: [
-                    babel({
-                        allowAllFormats: true,
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    modules: 'systemjs',
-                                    targets: {
-                                        browsers: legacySupport,
-                                    },
-                                },
-                            ],
-                        ],
-                    }),
-                ],
-            },
-            {
-                ...browserConfig,
-                format: 'amd',
-                exports: 'named',
-                file: `${outputDir}/bundle/amd/${filename}.js`,
-                plugins: [
-                    babel({
-                        allowAllFormats: true,
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    modules: 'amd',
-                                    targets: {
-                                        browsers: legacySupport,
-                                    },
-                                },
-                            ],
-                        ],
-                    }),
-                ],
-            },
-        ],
-    },
-]
+    })
+
+    return {
+        ...browserConfig,
+        name,
+        format,
+        exports: 'named',
+        file: `${outputDir}/bundle/${format}/${filename}.js`,
+        plugins: [babelPlugin],
+    }
+}
