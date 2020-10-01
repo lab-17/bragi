ec9759344bab2e22090e008a960312e1ddb218c5
Vinícius Carvalho <vinicius@criptext.com>
2020-09-30 21:07:52 -0300

    refactor(docs): split the large react components into smallers
    fix #93

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -6,18 +6,75 @@ import useBaseUrl from '@docusaurus/useBaseUrl'
 
 const features: any[] = []
 
-function Feature({ imageUrl, title, description }: any) {
-    const imgUrl = useBaseUrl(imageUrl)
+function FeatureText({ title, description }) {
     return (
-        <div>
-            {imgUrl && (
-                <div>
-                    <img src={imgUrl} alt={title} />
-                </div>
-            )}
+        <>
             <h3>{title}</h3>
             <p>{description}</p>
+        </>
+    )
+}
+
+function FeatureImage({ imageUrl, title }) {
+    const imgUrl = useBaseUrl(imageUrl)
+    return (
+        imageUrl && (
+            <div>
+                <img src={imgUrl} alt={title} />
+            </div>
+        )
+    )
+}
+function Feature({ imageUrl, title, description }) {
+    return (
+        <div>
+            <FeatureImage imageUrl={imageUrl} title={title} />
+            <FeatureText title={title} description={description} />
         </div>
+    )
+}
+
+function HomeHeroText({ siteConfig }) {
+    return (
+        <>
+            <h1>{siteConfig.title}</h1>
+            <p>{siteConfig.tagline}</p>
+        </>
+    )
+}
+
+function HomeHeroBtn() {
+    return (
+        <div>
+            <Link to={useBaseUrl('docs/')}>Get Started</Link>
+        </div>
+    )
+}
+
+function HomeHero({ siteConfig }) {
+    return (
+        <div>
+            <HomeHeroText siteConfig={siteConfig} />
+            <HomeHeroBtn />
+        </div>
+    )
+}
+
+function HomeHeader({ siteConfig }) {
+    return (
+        <header>
+            <HomeHero siteConfig={siteConfig} />
+        </header>
+    )
+}
+
+function HomeMain() {
+    return (
+        <main>
+            {features.map((props, idx) => (
+                <Feature key={idx} {...props} />
+            ))}
+        </main>
     )
 }
 
@@ -29,28 +86,8 @@ function Home() {
             title={`Hello from ${siteConfig.title}`}
             description="Description will go into a meta tag in <head />"
         >
-            <header>
-                <div>
-                    <h1>{siteConfig.title}</h1>
-                    <p>{siteConfig.tagline}</p>
-                    <div>
-                        <Link to={useBaseUrl('docs/')}>Get Started</Link>
-                    </div>
-                </div>
-            </header>
-            <main>
-                {features && features.length > 0 && (
-                    <section>
-                        <div>
-                            <div>
-                                {features.map((props, idx) => (
-                                    <Feature key={idx} {...props} />
-                                ))}
-                            </div>
-                        </div>
-                    </section>
-                )}
-            </main>
+            <HomeHeader siteConfig={siteConfig} />
+            <HomeMain />
         </Layout>
     )
 }
