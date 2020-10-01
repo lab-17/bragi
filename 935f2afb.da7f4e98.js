2d93e47b129cd5ac9144dc04a01dad2d3ee2eb35
Vinícius Carvalho <vinicius@criptext.com>
2020-10-01 00:22:02 +0000

    Deploying to docs from  @ ec9759344bab2e22090e008a960312e1ddb218c5 🚀

---
+++ 935f2afb.da7f4e98.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{63:function(e){e.exports=JSON.parse('{"version":"current","label":"Next","isLast":true,"docsSidebars":{},"permalinkToSidebar":{}}')}}]);
---
+++ img/LICENSE.md
@@ -0,0 +1,3 @@
+# Licenses
+
+-   The logo, favicon and any asset with Bragi's identity is **UNLICENSED**
