9da3b743f6a5bec4f22c9ffff6a70150aaa07fe3
Vinícius Carvalho <vinicius@criptext.com>
2020-10-04 20:49:25 -0300

    refactor: real implementation for logger

--- packages/bragi/src/util/logger.ts
+++ packages/bragi/src/util/logger.ts
@@ -1,3 +1,51 @@
-export const warn = console.warn as any
-export const info = console.info as any
-export const error = console.error as any
+import { safeGlobal } from '../config'
+
+const prefix = '[Bragi]:'
+
+const logLevels = {
+    error() {
+        return ['error']
+    },
+    warn() {
+        return ['error', 'warn']
+    },
+    info() {
+        return ['error', 'warn', 'info']
+    },
+    debug() {
+        return 'debug'
+    },
+    none() {
+        return []
+    },
+}
+
+const setLogLevel = (level: TBragiLoggerLevel): string[] | string => logLevels[level]()
+
+const useLog = (currentLogLevel: string[] | string, global = safeGlobal) => (
+    level: keyof typeof console,
+    ...data: unknown[]
+) => {
+    if (currentLogLevel === 'debug' || currentLogLevel.includes(level))
+        return global.console[level](prefix, ...data)
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function createLogger(level: TBragiLoggerLevel, global = safeGlobal) {
+    const currentLogLevel = setLogLevel(level)
+
+    const log = useLog(currentLogLevel, global)
+
+    return {
+        warn: (...data: unknown[]): void => log('warn', ...data),
+        info: (...data: unknown[]): void => log('info', ...data),
+        error: (...data: unknown[]): void => log('error', ...data),
+        debug: (
+            type: Exclude<keyof typeof console, TBragiLoggerLevel> | true,
+            ...data: unknown[]
+        ): void => log(type === true ? 'debug' : type, ...data),
+    }
+}
+
+export type TBragiLoggerLevel = keyof typeof logLevels
+export type IBragiLogger = ReturnType<typeof createLogger>
